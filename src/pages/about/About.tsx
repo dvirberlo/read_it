ee96d8a8df6aa222134410731b6f7d6441f04757
dvirberlo <dvir.berl@gmail.com>
2023-04-03 15:27:50 +0300

    feat: add app version in About page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,9 +1,10 @@
-import { description, repository } from "@/../package.json";
+import { description, repository, version } from "@/../package.json";
 export default function About() {
   return (
     <div className="mt-2 p-1">
       <h2 className="text-2xl">About</h2>
       <p className="p-2">{description}</p>
+      <p className="p-2">Version: {version}</p>
       <a
         className="cursor-pointer hover:text-primary4 text-content2 p-2"
         href={repository.url}
